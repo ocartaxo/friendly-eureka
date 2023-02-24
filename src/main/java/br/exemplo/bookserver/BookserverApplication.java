b8ebe408c5303c86d096db1d480d76939adee072
Otavio Cartaxo <oacartaxo@gmail.com>
2023-02-24 17:46:08 -0300

    aplicação

---
+++ src/main/java/br/exemplo/bookserver/BookserverApplication.java
@@ -0,0 +1,13 @@
+package br.exemplo.bookserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookserverApplication.class, args);
+    }
+
+}
