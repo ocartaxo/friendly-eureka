32a92d3b8293a91c605f23628e067db0dfe329fe
Otavio Cartaxo <oacartaxo@gmail.com>
2023-02-24 17:48:48 -0300

    teste (vazio) para aplicação

---
+++ src/test/java/br/exemplo/bookserver/BookserverApplicationTests.java
@@ -0,0 +1,13 @@
+package br.exemplo.bookserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
